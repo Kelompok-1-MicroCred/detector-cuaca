eae5c16fd27f9533d0d51b7eece16a5f29168e5c
Ammar Darma <65392193+ammaresok@users.noreply.github.com>
2021-12-12 02:06:11 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -8,9 +8,13 @@ from sklearn import metrics
 from sklearn.metrics import confusion_matrix
 import statsmodels.api as sm
 from sklearn import linear_model
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+
 
 app = Flask(__name__)
-model = pickle.load(open('trained_model.pkl', 'rb'))
+model = load_model('deeplearning.h5')
+
 
 @app.route('/')
 def home():
@@ -18,13 +22,22 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [x for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
-    output = round(prediction[0], 2)
+    output = prediction
 
-    return render_template("index.html", prediction_text='Your predicted annual Healthcare Expense is $ {}'.format(output))
+    wilayah = str(request.form['wilayah'])
+    waktu = str(request.form['waktu'])
+    kelembaban_persen = str(request.form['kelembaban_persen'])
+    suhu_derajat_celsius = str(request.form['suhu_derajat_celsius'])
+    banyakkotarawan = str(request.form['banyakkotarawan'])
+
+    data = [wilayah, waktu, kelembaban_persen, suhu_derajat_celsius, banyakkotarawan]
+
+    data_df = pd.DataFrame(data=data, columns=items)
+
+    return render_template("index.html", prediction_text='Cuaca yang akan terjadi nanti yaitu {}'.format(output))
 
 @app.route('/results',methods=['POST'])
 def results():
@@ -32,7 +45,7 @@ def results():
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
-    output = prediction[0]
+    output = prediction
     return jsonify(output)
 
 if __name__ == "__main__":
